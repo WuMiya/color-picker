89022a339133adf2c0ff4257a0edaf06c12010db
miyatruii <miya.wu@truii.com>
2018-09-14 11:53:25 +1000

    build a color picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
-
+import { ColorEvent } from 'ngx-color';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public color1: string = '#2889e9';
+  show: boolean= false;
+  public recentChoose: any[] = [];
+  selectedColour:any ="";
+  reuseColour:any ="";
+
+  cpPresetColors: any[] = ["#ea4256", "#ffa500", "#ffc966", "#f67d3b", "#43e5fd", "#20a5e6", "#1F917A", "#8B572A", 
+    "#000000","#4A4A4A", "#ffffff", "#9B9B9B"];
+
+
+  onSelect(colour) {
+    this.selectedColour = colour;
+  
+    if (this.recentChoose.includes(this.selectedColour) === false) {
+      this.recentChoose.unshift(this.selectedColour);
+    } else {
+      const index = this.recentChoose.findIndex( c => c === this.selectedColour)
+      this.recentChoose.splice(index,1);
+      this.recentChoose.unshift(this.selectedColour);
+    }
+
+    if (this.recentChoose.length > 6){
+      this.recentChoose.splice(-1,1);
+    }
+  }
+
+  title = 'app';
+  primaryColor = '#194D33';
+  state = {
+    h: 150,
+    s: 0.50,
+    l: 0.20,
+    a: 1,
+  };
+
+  changeComplete($event: ColorEvent) {
+    this.state = $event.color.hsl;
+    this.primaryColor = $event.color.hex;
+  }
+
+  reuseCol(recentCol) {
+    this.selectedColour = recentCol;
+    const index = this.recentChoose.findIndex( c => c === this.selectedColour)
+    this.recentChoose.splice(index,1);
+    this.recentChoose.unshift(this.selectedColour);
+  }
+
 }
+
+class showColourPicker {
+  show: boolean = true;
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { FormsModule } from '@angular/forms';
+import { ColorSketchModule } from 'ngx-color/sketch';
+import { ColorPhotoshopModule } from 'ngx-color/photoshop';
+import { ColorHueModule }  from 'ngx-color/hue';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ColorPickerModule,
+    FormsModule,
+    ColorSketchModule,
+    ColorPhotoshopModule,
+    ColorHueModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
